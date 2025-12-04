e007cbe4302ad960984208b231f610513d6aef80
omar-hanafy <omar_hanafy@icloud.com>
2025-12-05 00:03:04 +0200

    docs: Add library-level documentation to marker_widget.dart

--- lib/marker_widget.dart
+++ lib/marker_widget.dart
@@ -1,4 +1,13 @@
-/// todo: add docs.
+/// A Flutter package for rendering widgets as Google Maps markers.
+///
+/// This library provides tools to convert any Flutter [Widget] into a
+/// [BitmapDescriptor] or [BytesMapBitmap] that can be used with the
+/// `google_maps_flutter` package.
+///
+/// Key features:
+/// * [toMarkerBitmap] extension on [Widget] for easy conversion.
+/// * [MarkerIconRenderer] for advanced control and caching.
+/// * Support for device pixel ratio and logical sizing.
 library;
 
 export 'src/marker_widget.dart';
